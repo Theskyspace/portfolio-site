44512babbceea3737d7dcfdce6c809440af296ec
Akash Joshi <akashrockzz411@gmail.com>
2023-06-17 22:47:01 +0530

    Intial: Setup and basic configuration.

---
+++ src/components/AboutSection.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import './AboutSection.css'
+
+export default function AboutSection() {
+  return (
+    <div className='AboutSection-content'>AboutSection</div>
+  )
+}
---
+++ src/components/HeroSection.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './HeroSection.css'
+
+
+export default function HeroSection() {
+  return (
+    <div className='HeroSection-content'>HeroSection</div>
+  )
+}
