293add3d8fd1a1784b0c1da841c50fa63a949e2a
Akash Joshi <akashrockzz411@gmail.com>
2023-06-18 00:28:03 +0530

    feat : Adding current Progress in Hero Section.
    - Added Texts and root variable in CSS.
    - Formatted the text.
    - Partially responsive.

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -4,6 +4,13 @@ import './HeroSection.css'
 
 export default function HeroSection() {
   return (
-    <div className='HeroSection-content'>HeroSection</div>
+    <div className='HeroSection-content'>
+      <div className="content">
+        <h4 className='sub-subheading'>Hello world, My name is</h4>
+        <h1 className='heading'>Akash Joshi</h1>
+        <h1 className='subheading'>I build things for the world.</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Viverra nam libero justo laoreet sit amet cursus.</p>
+      </div>
+    </div>
   )
 }
