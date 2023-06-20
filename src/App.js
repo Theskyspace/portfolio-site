47de6cdb24ca7624497f3d65b802944f31d20b89
Akash Joshi <43512801+Theskyspace@users.noreply.github.com>
2023-06-20 21:23:27 +0530

    About page (#3)
    * Feat: Completed the About Page. Finishing is pending.
    
    * Fix : Content and final touches.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import AboutSection from './components/AboutSection';
 import HeroSection from './components/HeroSection';
 import NavBar from './components/NavBar';
+import Experience from './components/experience';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <NavBar />
       <HeroSection />
       <AboutSection />
+      <Experience />
     </div>
   );
 }
---
+++ src/components/experience.js
@@ -0,0 +1,9 @@
+import './experience.css';
+
+export default function Experience() {
+    return (
+        <div className="ExperienceSection" id='ExperienceSection'>
+            <p>Experience Section under progress</p>
+        </div>
+    )
+}
