daba7a3693750d5887d2a0b8cb2e11c502a734f8
Akash Joshi <akashrockzz411@gmail.com>
2023-06-21 11:04:19 +0530

    Contact Page intial layout.
    - Adding responsiveness is left.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -36,9 +36,9 @@ export default function NavBar() {
             <div className={clicked && visible ? "navbar-list navbar-active" : "navbar-list"}>
                 <ul style={{animationDelay : "0ms"}} className='fadedown-enter-done'>01. <Link to="AboutSection-content" spy={true} smooth={true} offset={50} duration={500} >About</Link> </ul>
                 <ul style={{animationDelay : "100ms"}} className='fadedown-enter-done'>02. <Link to="ExperienceSection" spy={true} smooth={true} offset={50} duration={500} >Experience</Link> </ul>
-                <ul style={{animationDelay : "200ms"}} className='fadedown-enter-done'>03. <a href="http://">Work</a></ul>
-                <ul style={{animationDelay : "300ms"}} className='fadedown-enter-done'>04. <a href="http://">Contact</a></ul>
-                <ul style={{animationDelay : "400ms"}} className='fadedown-enter-done'><a href=''><span className='Resume-button'>Resume</span></a></ul>
+                <ul style={{animationDelay : "200ms"}} className='fadedown-enter-done'>03. <Link to="ExperienceSection" spy={true} smooth={true} offset={50} duration={500} >Work</Link></ul>
+                <ul style={{animationDelay : "300ms"}} className='fadedown-enter-done'>04.  <Link to="ContactSection" spy={true} smooth={true} offset={50} duration={500} >Contact</Link></ul>
+                <ul style={{animationDelay : "400ms"}} className='fadedown-enter-done'><a href='https://'><span className='Resume-button'>Resume</span></a></ul>
             </div>
             <div className='mobile' onClick={handleClick}>
             </div>
