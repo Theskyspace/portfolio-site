97e28c4ec5c5c5630a97e2db2bd88a363fdfff44
Akash Joshi <akashrockzz411@gmail.com>
2023-06-22 11:49:26 +0530

    Project Section responsiveness done.

--- src/components/work.js
+++ src/components/work.js
@@ -6,101 +6,79 @@ export default function Work() {
   return (
     <section className='Work-section Section-content' id='Work-section'>
       <h2 className='Section-heading'><span>03.</span> Some Things I’ve Built</h2>
-      <div className="featured-projects">
-        <div className="project">
-          <div className="project-image">
-            <img src={project1Image} alt="" />
-          </div>
+      <ul className="featured-projects">
+        <li className='featured-tile'>
           <div className="project-content">
-            <h4>Featured Projects</h4>
-            <h3>NeoEdu</h3>
-            <div className='Project-decs'>
-              <p>A minimal, dark blue theme for VS Code, Sublime Text, Atom, iTerm, and more. Available on Visual Studio Marketplace, Package Control, Atom Package Manager, and npm.</p>
-            </div>
-            <div className="tech-used">
-              <p className='tech-used-tile'>c++</p>
-              <p className='tech-used-tile'>Python</p>
-              <p className='tech-used-tile'>Java</p>
-            </div>
-            <div className="project-links">
-              <a href="https://github.com/theskyspace" target="_blank" rel="noreferrer">
-                <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" strokeLinecap="round" className="feather feather-Github">
-                  <title>Github</title>
-                  <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
-                </svg>
-              </a>
-              <a href="https://github.com/theskyspace" target="_blank" rel="noreferrer">
-                <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" strokeLinecap="round" className="feather feather-Github">
-                  <title>Github</title>
-                  <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="currentColor" className="feather feather-external-link"><title>External Link</title><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path><polyline points="15 3 21 3 21 9"></polyline><line x1="10" y1="14" x2="21" y2="3"></line></svg>
-                </svg>
-              </a>
+            <div>
+              <p className='featured-title'>Featured Projects</p>
+              <h3 className="project-title">NeoEdu</h3>
+              <div className="project-decs">
+                <p>EA minimal, dark blue theme for VS Code, Sublime Text, Atom, iTerm, and more. Available on Visual Studio Marketplace, Package Control, Atom Package Manager, and npm.</p>
+              </div>
+              <ul className="project-tech-list">
+                <li>VSCode</li>
+                <li>Python</li>
+                <li>VSCode</li>
+                <li>Python</li>
+              </ul>
             </div>
           </div>
-        </div>
-        <div className="project left">
-          <div className="project-content project-content-right">
-            <h4>Featured Projects</h4>
-            <h3>NeoEdu</h3>
-            <div className='Project-decs'>
-              <p>A minimal, dark blue theme for VS Code, Sublime Text, Atom, iTerm, and more. Available on Visual Studio Marketplace, Package Control, Atom Package Manager, and npm.</p>
-            </div>
-            <div className="tech-used tech-used-left">
-              <p className='tech-used-tile'>c++</p>
-              <p className='tech-used-tile'>Python</p>
-              <p className='tech-used-tile'>Java</p>
-            </div>
-            <div className="project-links">
-              <a href="https://github.com/theskyspace" target="_blank" rel="noreferrer">
-                <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" strokeLinecap="round" className="feather feather-Github">
-                  <title>Github</title>
-                  <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
-                </svg>
-              </a>
-              <a href="https://github.com/theskyspace" target="_blank" rel="noreferrer">
-                <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" strokeLinecap="round" className="feather feather-Github">
-                  <title>Github</title>
-                  <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="currentColor" className="feather feather-external-link"><title>External Link</title><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path><polyline points="15 3 21 3 21 9"></polyline><line x1="10" y1="14" x2="21" y2="3"></line></svg>
-                </svg>
-              </a>
-            </div>
-          </div>
-          <div className="project-image project-image-left">
-            <img src={project1Image} alt="" />
-          </div>
-        </div>
-        <div className="project">
           <div className="project-image">
-            <img src={project1Image} alt="" />
+            <div className="image-wrapper">
+              <img src={project1Image} alt="" />
+
+            </div>
           </div>
+        </li>
+        <li className='featured-tile'>
           <div className="project-content">
-            <h4>Featured Projects</h4>
-            <h3>NeoEdu</h3>
-            <div className='Project-decs'>
-              <p>A minimal, dark blue theme for VS Code, Sublime Text, Atom, iTerm, and more. Available on Visual Studio Marketplace, Package Control, Atom Package Manager, and npm.</p>
-            </div>
-            <div className="tech-used">
-              <p className='tech-used-tile'>c++</p>
-              <p className='tech-used-tile'>Python</p>
-              <p className='tech-used-tile'>Java</p>
-            </div>
-            <div className="project-links">
-              <a href="https://github.com/theskyspace" target="_blank" rel="noreferrer">
-                <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" strokeLinecap="round" className="feather feather-Github">
-                  <title>Github</title>
-                  <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
-                </svg>
-              </a>
-              <a href="https://github.com/theskyspace" target="_blank" rel="noreferrer">
-                <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" strokeLinecap="round" className="feather feather-Github">
-                  <title>Github</title>
-                  <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="currentColor" className="feather feather-external-link"><title>External Link</title><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path><polyline points="15 3 21 3 21 9"></polyline><line x1="10" y1="14" x2="21" y2="3"></line></svg>
-                </svg>
-              </a>
+            <div>
+              <p className='featured-title'>Featured Projects</p>
+              <h3 className="project-title">NeoEdu</h3>
+              <div className="project-decs">
+                <p>EA minimal, dark blue theme for VS Code, Sublime Text, Atom, iTerm, and more. Available on Visual Studio Marketplace, Package Control, Atom Package Manager, and npm.</p>
+              </div>
+              <ul className="project-tech-list">
+                <li>VSCode</li>
+                <li>Python</li>
+                <li>VSCode</li>
+                <li>Python</li>
+              </ul>
             </div>
           </div>
-        </div>
-      </div>
+          <div className="project-image">
+            <div className="image-wrapper">
+              <img src={project1Image} alt="" />
+
+            </div>
+          </div>
+        </li>
+        <li className='featured-tile'>
+          <div className="project-content">
+            <div>
+              <p className='featured-title'>Featured Projects</p>
+              <h3 className="project-title">NeoEdu</h3>
+              <div className="project-decs">
+                <p>EA minimal, dark blue theme for VS Code, Sublime Text, Atom, iTerm, and more. Available on Visual Studio Marketplace, Package Control, Atom Package Manager, and npm.</p>
+              </div>
+              <ul className="project-tech-list">
+                <li>VSCode</li>
+                <li>Python</li>
+                <li>VSCode</li>
+                <li>Python</li>
+              </ul>
+            </div>
+          </div>
+          <div className="project-image">
+            <div className="image-wrapper">
+              <img src={project1Image} alt="" />
+
+            </div>
+          </div>
+        </li>
+
+        
+      </ul>
     </section>
 
   )
