8f93d56b208c648adf55fa64f7beffc2ebd1ce43
Akash Joshi <43512801+Theskyspace@users.noreply.github.com>
2023-11-08 20:24:34 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -5,10 +5,24 @@ import NavBar from './components/NavBar';
 import Experience from './components/experience';
 import Contact from './components/contact';
 import Work from './components/work';
+import { BrowserRouter , Route,  Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="HeroSection">     
+  <>
+    <BrowserRouter>
+      <Switch >
+        <Route exact path="/" component={Home} />
+        <Route exact path="/resume" component={resume} />
+      </Switch  >
+    </BrowserRouter>
+  </>
+  );
+}
+function Home() {
+  console.log("Here")
+  return (
+    <div className="HeroSection">
       <NavBar />
       <HeroSection />
       <AboutSection />
@@ -16,7 +30,13 @@ function App() {
       <Work />
       <Contact />
     </div>
-  );
+  )
 }
+const resume = () => {
+  // Replace "resume.pdf" with the path to your PDF file
+  const pdfUrl = "/static/media/resume.3d583d6ea6a6636c1048.pdf";
+  window.location.href = pdfUrl;
+  return null; // Return null to prevent rendering anything for this component
+};
 
 export default App;
